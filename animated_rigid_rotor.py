e1130d7aebebcca3383ec28a0008eaa36163a309
FoleyLab <jayfoleyiv@gmail.com>
2018-04-02 18:52:23 -0400

    Found error in FourierAnalysis
    Complex conjugate of RR energy eigenfunctions needed to get expansion coefficients from the Fourier integral; this was not a problem with the PIB because the energy eigenfunctions were real valued.

--- animated_rigid_rotor.py
+++ animated_rigid_rotor.py
@@ -6,23 +6,25 @@ fig = plt.figure()
 ax = plt.axes(xlim=(0, 2*np.pi), ylim=(-5, 5))
 line, = ax.plot([], [], lw=2)
 
+### array of angle values
 x = np.linspace(0,np.pi*2,500)
 
 
-
+### pass array of angles (phi), central angle (phi0), spread (sig), and mean
+### angular momentum (k0)
 def Gauss_Packet(phi, phi0, sig, k0):
     ci = 0.+1j
     pre = 1./(sig*np.sqrt(2.*np.pi))
-    psi_x = pre*np.exp(-0.5*((phi-phi0)/sig)**2)*np.exp(ci*l0*phi)
+    psi_x = pre*np.exp(-0.5*((phi-phi0)/sig)**2)*np.exp(ci*k0*phi)
     return psi_x
 
 def rigid_eigenfunc (phi, ml):
    ci = 0. + 1j
-   return (1/np.sqrt(np.pi*2)*np.exp(ci*ml*phi))
+   return (1/np.sqrt(np.pi*2))*np.exp(ci*ml*phi)
 
 
 def rigid_energy (ml):
-    return ((ml*ml))/(2*mu*(r*r))
+    return ml*ml/(2*mu*r*r)
 
 def rigid_time (ml, t):
     ci = 0. + 1j
@@ -30,14 +32,18 @@ def rigid_time (ml, t):
 
 def FourierAnalysis(phi, Psi, ml):
     cn = np.zeros(len(ml),dtype=complex)
-    dphi = phi[1]-phi[0]
+    ### dphi can be gotten from any two elements of phi bc it is a linspace array
+    dphi = phi[1] - phi[0]
     for i in range (0,len(ml)):
 
       som = 0+0j
       psi_i = rigid_eigenfunc (phi, ml[i])
 
+      ### very important to take the complex conjugate of psi_i in the following integral!!!
+      ### was not necessary in the PIB case because the energy eigenfunctions were real valued, but
+      ### RR energy eigenfunctions are complex
       for j in range (0, len(phi)):
-        som = som + psi_i[j]*Psi[j]*dphi
+        som = som + np.conj(psi_i[j])*Psi[j]*dphi
 
       cn[i] = som
 
@@ -45,24 +51,15 @@ def FourierAnalysis(phi, Psi, ml):
 
 
 
-#nt = np.linspace(1,200,200)
-
-#p1 = rigid_eigenfunc(rad, 1)
-### xt doesn't seem to be needed
-#xt = np.linspace(0, 200, 200)
-#P1 = rigid_energy(1)*rigid_eigenfunc(rad, 1)
-P1 = rigid_eigenfunc(x, 1)
-sig = 0.2
-nt=np.linspace(-20, 20, 41)
+sig = 0.1
+nt=np.linspace(-100, 100, 201)
 phi0 = np.pi/3
 m1 = 1
 m2 = 1
 mu = (m1*m2)/(m1+m2)
 r = 1.39839789
-k0 = 0
-l0 = 0
-## don't think phi is needed because rad is defined already
-#phi = np.linspace(0, 2*np.pi, 200)
+k0 = 5*np.pi
+
 Psi = Gauss_Packet(x, phi0, sig, k0)
 cn = FourierAnalysis(x, Psi, nt)
 print(cn)
@@ -70,29 +67,19 @@ print(cn)
 psi_exp = np.zeros_like(Psi)
 
 for i in range(0,len(cn)):
-  psi_exp = psi_exp + cn[i]*rigid_eigenfunc(x, i)
+  psi_exp = psi_exp + cn[i]*rigid_eigenfunc(x, nt[i])
 
-plt.plot(x, np.real(Psi), 'red', x, np.real(psi_exp), 'b--')
-plt.show()
-#rigid_eigenfunc (phi, ml)
-#y=eigenfunc(phi, ml)*energyfunc(ml)
-#x= np.real(r*np.cos(phi))
-#y= np.real(r*np.sin(phi))
-
-
-
-#### Static plot
-#plt.plot(rad, np.real(P1))
+#plt.plot(x, np.real(Psi), 'red', x, np.real(psi_exp), 'b--')
 #plt.show()
-#print(P1)
 
 def init():
     line.set_data([], [])
     return line,
 
 # animation function.  This is called sequentially to generate the animation
+'''  #Delete the three quote marks to uncomment this animation section
 def animate(i):
-
+    
     ### Once PIB_Func and PIB_En are defined, the following
     ### code can be used to plot the time-evolution of an energy eigenfunction
 
@@ -103,23 +90,25 @@ def animate(i):
     #m = np.linspace(-5, 5, 11)
     ### Imaginary unit i
     ci = 0.+1j
-    psi_t = np.zeros(len(x),dtype=complex)
-    for j in range(0,len(nt)):
+    psi_t = np.zeros_like(Psi)
+    for j in range(0,len(cn)):
       p1 = rigid_eigenfunc(x, nt[j])
-      ft  = rigid_time(nt[j], i/10)
+      ft  = rigid_time(nt[j], i/100)
       psi_t = psi_t + cn[j]*p1*ft
-
+   
     psi_t_star = np.conj(psi_t)
 
     y = np.real(psi_t)
     z = np.imag(psi_t)
     p = np.real(psi_t_star * psi_t)
-    line.set_data(x, y)
+    line.set_data(x, p)
     return line,
 
 
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=10000, interval=100, blit=True)
+                               frames=10000, interval=200, blit=True)
 ### uncomment to save animation as mp4 
 #anim.save('pib_wp.mp4', fps=20, extra_args=['-vcodec', 'libx264'])
 plt.show()
+
+'''
